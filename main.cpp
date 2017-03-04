331f538aec66943a0a03bfde8b56d88f009fc131
BarredGalaxy <BarredGalaxy@users.noreply.github.com>
2017-03-04 15:18:50 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char user[25] = "memes";
+    int number = 0;
+
+
+    cout << "Choose a Number: ";
+    cin >> number;
+    cout << number << ". Was This Your Number? Enter Another";
+    
+    return 0;
+}
